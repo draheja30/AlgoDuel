9cdc15bd84397c998e7380a5c32f4863f5d8afc8
Devansh Raheja <draheja@umass.edu>
2023-11-12 04:23:10 -0500

    authentication

--- server.js
+++ server.js
@@ -1,37 +1,45 @@
 import express, { json } from 'express';
-import { python, node} from 'compile-run';
+import { auth } from 'express-openid-connect';
 import morgan from 'morgan';
 import cors from 'cors';
+import { python, node } from 'compile-run';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
 
-import { auth } from 'express-openid-connect';
 
 const app = express();
 
-const config = {
-  authRequired: false,
-  auth0Logout: true,
-  secret: 'a long, randomly-generated string stored in env',
-  baseURL: 'http://localhost:5173',
-  clientID: 'zFg49KoSJoGr4DT3pv2On93a9KTCD2oI',
-  issuerBaseURL: 'https://dev-040rb7jgqzzejfj7.us.auth0.com'
-};
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-// auth router attaches /login, /logout, and /callback routes to the baseURL
+// Auth0 configuration
+const config = {
+    authRequired: false,
+    auth0Logout: true,
+    secret: 'a long, randomly-generated string stored in env',
+    baseURL: 'http://localhost:3001',
+    clientID: 'ifjd3TisWmTqtYxof5wyQqg2LwkGTlHs',
+    issuerBaseURL: 'https://dev-040rb7jgqzzejfj7.us.auth0.com'
+    };
+// Auth router attaches /login, /logout, and /callback routes to the baseURL
 app.use(auth(config));
 
-// req.isAuthenticated is provided from the auth router
+// Modified root route
 app.get('/', (req, res) => {
-  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+  if (req.oidc.isAuthenticated()) {
+    // Redirect to the specified URL upon successful login
+    res.redirect('http://localhost:5173');
+  } else {
+    // User not authenticated, show logged out message
+    res.sendFile(join(__dirname, 'Home.html'));
+  }
 });
 
-
-
-
 app.use(json()); // For parsing application/json
-
 app.use(morgan('dev'));
 app.use(cors());
 
+// Endpoint to run code
 app.post('/run-code', async (req, res) => {
     const { language, code } = req.body;
 
@@ -39,7 +47,6 @@ app.post('/run-code', async (req, res) => {
         let result;
         if (language === 'Python') {
             result = await python.runSource(code);
-            console.log(code);
         } else if (language === 'Javascript') {
             result = await node.runSource(code);
         } else {
@@ -53,6 +60,7 @@ app.post('/run-code', async (req, res) => {
     }
 });
 
+// Start the server
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
