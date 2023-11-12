ff71206bd796da5daff5e0c572feb6fd1b906338
Devansh Raheja <draheja@umass.edu>
2023-11-11 22:45:30 -0500

    Auth

--- server.js
+++ server.js
@@ -2,7 +2,30 @@ import express, { json } from 'express';
 import { python, node} from 'compile-run';
 import morgan from 'morgan';
 import cors from 'cors';
+
+import { auth } from 'express-openid-connect';
+
 const app = express();
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: 'a long, randomly-generated string stored in env',
+  baseURL: 'http://localhost:5173',
+  clientID: 'zFg49KoSJoGr4DT3pv2On93a9KTCD2oI',
+  issuerBaseURL: 'https://dev-040rb7jgqzzejfj7.us.auth0.com'
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
+app.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
+
+
+
 
 app.use(json()); // For parsing application/json
 
